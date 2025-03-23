2a187abc052a6250d7bf2a5028d67915e3b21ea2
pp123pp <1217272252@qq.com>
2025-03-23 20:29:30 +0800

    加入拖拽

---
+++ src/renderer/src/components/Draggable/index.tsx
@@ -0,0 +1,24 @@
+import { useDraggable } from "@dnd-kit/core";
+
+export function Draggable(props) {
+    const { attributes, listeners, setNodeRef, transform } = useDraggable({
+        id: "draggable",
+    });
+
+    // attributes： 一个对象，包含使组件可拖动所需的 HTML 属性
+    // listeners： 包含用户拖动组件时触发的事件监听器的对象
+    // setNodeRef： 用于设置组件 DOM 节点引用的函数
+    // transform： 一个对象，包含组件当前的 x 和 y 移位（以像素为单位）
+
+    const style = transform
+        ? {
+              transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+          }
+        : undefined;
+
+    return (
+        <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
+            {props.children}
+        </button>
+    );
+}
--- src/renderer/src/components/LayerManagement/index.tsx
+++ src/renderer/src/components/LayerManagement/index.tsx
@@ -1,6 +1,5 @@
-import { DndContext, useDraggable } from "@dnd-kit/core";
 import classNames from "classnames";
-import { CSSProperties, memo, useState } from "react";
+import { CSSProperties, memo } from "react";
 import { Wrapper } from "./style";
 
 export default memo(function ({
@@ -10,33 +9,9 @@ export default memo(function ({
     style?: CSSProperties;
     className?: string;
 }) {
-    const { attributes, listeners, setNodeRef, transform } = useDraggable({
-        id: "draggable",
-    });
-
-    const [originTransform, setOriginTransform] = useState({
-        transform,
-    });
-
-    if (!transform) {
-        debugger;
-    }
-
-    const style2 = transform
-        ? {
-              transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
-          }
-        : undefined;
-
     return (
-        <DndContext>
-            <Wrapper
-                ref={setNodeRef}
-                style={style2}
-                {...listeners}
-                {...attributes}
-                className={classNames(className)}
-            ></Wrapper>
-        </DndContext>
+        <Wrapper style={style} className={classNames(className)}>
+            拖动我
+        </Wrapper>
     );
 });
